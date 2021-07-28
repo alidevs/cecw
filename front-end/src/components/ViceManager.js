c3e821f8946f982b5682783e285206e0ce7d3866
MBSALDarwishs <z1e2d3@hotmail.com>
2021-07-28 07:30:02 +0300

    ar*trans 2

--- front-end/src/components/ViceManager.js
+++ front-end/src/components/ViceManager.js
@@ -49,13 +49,13 @@ export default class ViceManager extends Component {
                             <div style={{textAlign: 'center'}} className="each-icon box-4">
                               <div className="icon-wrap">
                                 <i className="fa fa-list-ul" />
-                                <h3 style={{color: '#F3EFE3'}}>Check Employees Custody Requests</h3>
+                                <h3 style={{color: '#F3EFE3'}}>التحقق من عٌهد الموظفين</h3>
                               </div>
                               <div className="icon-text">
-                                <h3>Check Employees Custody Requests...des</h3>
-                                <p>Check Employees Custody Requests...des</p>
+                              <h3>التحقق من عٌهد الموظفين</h3>
+                            <p>قبول او رفض طلبات العٌهد</p>
                                 <div className="cta">
-                                  <a title="Click to go to..." href="javascript:void(0);"><i className="fa fa-arrow-right" aria-hidden="true" /></a>
+                                <a title="اضغط هنا للذهاب للتحقق من عٌهد الموظفين" href="/#"><i className="fa fa-arrow-right" aria-hidden="true" />&nbsp;</a>
                                 </div>
                               </div>
                             </div>
@@ -66,14 +66,15 @@ export default class ViceManager extends Component {
                             <div style={{textAlign: 'center'}} className="each-icon box-3">
                               <div className="icon-wrap">
                                 <i className="fas fa-plus-circle" />
-                                <h3 style={{color: '#F3EFE3'}}>Items Menu</h3>
+                                <h3 style={{color: '#F3EFE3'}}>قائمة العناصر</h3>
                               </div>
                               <div className="icon-text">
-                                <h3>Items Menu...des</h3>
-                                <p>Items Menu...des</p>
-                                <div className="cta">
-                                  <a title="Click to go to..." href="javascript:void(0);"><i className="fa fa-arrow-right" aria-hidden="true" /></a>
-                                </div>
+                              <h3>قائمة العناصر</h3>
+                            <p>لعرض جميع عناصر المستودعات</p>
+                            <div className="cta">
+                              <a title="اضغظ هنا للذهاب لقائمة العناصر" href="/#"><i className="fa fa-arrow-right" aria-hidden="true" />&nbsp;</a>
+
+                            </div>
                               </div>
                             </div>
                             </Link>
@@ -83,14 +84,14 @@ export default class ViceManager extends Component {
                             <div style={{textAlign: 'center'}} className="each-icon box-2">
                               <div className="icon-wrap">
                                 <i className="fa fa-history" />
-                                <h3 style={{color: '#F3EFE3'}}>Custody History</h3>
-                              </div>
-                              <div className="icon-text">
-                                <h3>Custody History...des</h3>
-                                <p>Custody History...des</p>
-                                <div className="cta">
-                                  <a title="Click to go to..." href="javascript:void(0);"><i className="fa fa-arrow-right" aria-hidden="true" /></a>
-                                </div>
+                                <h3 style={{color: '#F3EFE3'}}>سجلات العُهد</h3>
+                          </div>
+                          <div className="icon-text">
+                            <h3>سجلات العُهد</h3>
+                            <p>لعرض جميع طلبات العهد</p>
+                            <div className="cta">
+                              <a title="اضغط هنا للذهاب لسجلات العٌهد" href="/#"><i className="fa fa-arrow-right" aria-hidden="true" />&nbsp;</a>
+                            </div>
                               </div>
                             </div>
                             </Link>
@@ -100,13 +101,13 @@ export default class ViceManager extends Component {
                             <div style={{textAlign: 'center'}} className="each-icon box-1">
                               <div className="icon-wrap">
                                 <i className="fas fa-check-double" />
-                                <h3 style={{color: '#F3EFE3'}}>Request/Return Custody Items</h3>
+                                <h3 style={{color: '#F3EFE3'}}>طلب او إرجاع عٌهدة</h3>
                               </div>
                               <div className="icon-text">
-                                <h3>Request/Return Custody Items...des</h3>
-                                <p>Request/Return Custody Items...des</p>
+                                <h3>طلب او إرجاع عٌهدة</h3>
+                                <p>تمكنك من تقديم طلب او إرجاع العهد الخاصة بك</p>
                                 <div className="cta">
-                                  <a title="Click to go to..." href="javascript:void(0);"><i className="fa fa-arrow-right" aria-hidden="true" /></a>
+                                  <a title="إضغظ هنا للإنتقال لصفحة الطلب او الإسترجاع" href="javascript:void(0);"><i className="fa fa-arrow-right" aria-hidden="true" /></a>
                                 </div>
                               </div>
                             </div>
@@ -117,14 +118,14 @@ export default class ViceManager extends Component {
                             <div style={{textAlign: 'center', width: '51rem'}} className="each-icon box-2">
                               <div className="icon-wrap">
                                 <i className="fas fa-cubes" />
-                                <h3 style={{color: '#F3EFE3'}}>My Custody Items</h3>
-                              </div>
-                              <div className="icon-text">
-                                <h3>My Custody Items...des</h3>
-                                <p>My Custody Items...des</p>
-                                <div className="cta">
-                                  <a title="Click to go to..." href="javascript:void(0);"><i className="fa fa-arrow-right" aria-hidden="true" /></a>
-                                </div>
+                                <h3 style={{color: '#F3EFE3'}}>العُهد الخاصة بي</h3>
+                          </div>
+                          <div className="icon-text">
+                            <h3>العُهد الخاصة بي</h3>
+                            <p>تمكنك من عرض العٌهد الخاصة بك</p>
+                            <div className="cta">
+                              <a title="اضغط هنا للذهاب للعٌهد الخاصة بك" href="/#"><i className="fa fa-arrow-right" aria-hidden="true" />&nbsp;</a>
+                            </div>
                               </div>
                             </div>
                             </Link>
